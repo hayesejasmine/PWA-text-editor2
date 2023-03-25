09055ddb136ec4f6211e5bd8e56dabb79d9681d8
hayesejasmine <hayesejasmine@gmail.com>
2023-03-25 19:31:38 -0400

    webpack file updated

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -17,13 +17,62 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
+    devServer: {
+        hot: 'only',
+    },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+          template: './index.html',
+          title: 'Webpack Plugin'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WorkboxPlugin.GenerateSW(),
+
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Note taking app',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('src', 'icons'),
+          },
+        ],
+        orientation: "portrait",
+        display: "standalone",
+      }),
+
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
